1489a6dd50a0d49ab8200210c5ffd992524a65fb
Pedro Machado <pedrobmachado@hotmail.com>
2023-03-13 12:42:34 -0300

    project done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const secondContainer = document.querySelector('#seconds');
 const minutesContainer = document.querySelector('#minutes');
+const hoursContainer = document.querySelector('#hours');
+const daysContainer = document.querySelector('#days');
 
 
 const countDownDate = new Date('July 31 2023 00:00:00');
@@ -14,6 +16,8 @@ const UpdateCountDown = () =>{
 
 secondContainer.textContent = seconds;
 minutesContainer.textContent = minutes;
+hoursContainer.textContent = hours;
+daysContainer.textContent = days;
 }
 
 setInterval(UpdateCountDown, 1000 )
